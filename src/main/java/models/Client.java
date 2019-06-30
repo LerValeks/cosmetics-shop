09067b655f328ce422a079ababca6473d817e8f7
Danylo Tkachenko <d.y.tkachenko@gmail.com>
2019-06-30 16:44:00 +0200

    Cleanup of Client / Employee / Reservation classes

--- src/main/java/models/Client.java
+++ src/main/java/models/Client.java
@@ -4,7 +4,7 @@ import static models.UserCategory.CLIENT;
 
 public class Client extends User {
 
-    public Client(String name, String surname, String phoneNumber, UserCategory userCategory) {
+    public Client(String name, String surname, String phoneNumber) {
         super(name, surname, phoneNumber, CLIENT);
     }
 }
