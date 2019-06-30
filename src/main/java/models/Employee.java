7763c683a07b6ad9e7fa35d14807782199085748
Danylo Tkachenko <d.y.tkachenko@gmail.com>
2019-06-30 20:43:14 +0200

    Employee Class updated. Reservations class field added -> to store all reservations of employee

--- src/main/java/models/Employee.java
+++ src/main/java/models/Employee.java
@@ -1,13 +1,19 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static models.UserCategory.EMPLOYEE;
 
 public class Employee extends User {
+
+    List<Reservation> reservations;
 
     private ServiceCategory serviceCategory;
 
     public Employee(String name, String surname, String phoneNumber, ServiceCategory serviceCategory) {
         super(name, surname, phoneNumber, EMPLOYEE);
         this.serviceCategory = serviceCategory;
+        this.reservations = new ArrayList<>();
     }
 }
