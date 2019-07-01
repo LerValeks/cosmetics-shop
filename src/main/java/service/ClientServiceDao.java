a65ece563d671f1156767f83882d5675bffa461f
Aleks <aleksvaluyskov@gmail.com>
2019-07-01 21:37:59 +0300

    Adding Service Interface

---
+++ src/main/java/service/ClientServiceDao.java
@@ -0,0 +1,23 @@
+package service;
+
+import models.Client;
+import models.Reservation;
+import models.ServiceCategory;
+import repository.ClientDAO;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface ClientServiceDao {
+    Client add(Client client);
+
+    Client update(Client employee);
+
+    Client delete(Client employee);
+
+    List<Reservation> reservationByClient(ClientDAO clientDAO);
+
+    List<Reservation> reservationByClientInSpecificPeriod(Client client, LocalDateTime startDate, LocalDateTime endDate);
+
+
+}
---
+++ src/main/java/service/EmployeeServiceDAO.java
@@ -0,0 +1,27 @@
+package service;
+
+import models.Employee;
+import models.Reservation;
+import models.ServiceCategory;
+import repository.EmployeeDAO;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface EmployeeServiceDAO {
+    Employee add(Employee employee);
+
+    Employee update(Employee employee);
+
+    Employee delete(Employee employee);
+
+    List<Employee> ServiceCategoryEmployees(ServiceCategory serviceCategory, EmployeeDAO employeeDAO);
+
+    List<Employee> FreeEmployees(ServiceCategory serviceCategory, EmployeeDAO employeeDAO, Reservation reservation);
+
+    List<Reservation> reservationByEmployeeInSpecificPeriod(Employee employee, LocalDateTime startDate, LocalDateTime endDate);
+
+    List<Reservation> reservationByEmployeeByServiceCategoryInSpecificPeriod(Employee employee, LocalDateTime startDate, LocalDateTime endDate, ServiceCategory serviceCategory);
+
+
+}
