c76aa238dc6141256243ee02dc055791896c8e43
Danylo Tkachenko <d.y.tkachenko@gmail.com>
2019-07-04 17:06:56 +0200

    Reservation service impl added

--- src/main/java/service/ReservationServiceImpl.java
+++ src/main/java/service/ReservationServiceImpl.java
@@ -10,19 +10,47 @@ import java.util.Set;
 
 public class ReservationServiceImpl implements ReservationService {
 
-    EmployeeDatabase employeeDatabase;
+    private EmployeeDatabase employeeDatabase;
+    private ClientDatabase clientDatabase;
 
-    public ReservationServiceImpl(EmployeeDatabase employeeDatabase) {
+    //TODO: Robert to advise if 2 constructors are needed? Or one with 2 database parameters? How this may affect?
+
+    public ReservationServiceImpl(EmployeeDatabase employeeDatabase, ClientDatabase clientDatabase) {
         this.employeeDatabase = employeeDatabase;
+        this.clientDatabase = clientDatabase;
     }
 
     @Override
     public Reservation bookVisit(Reservation reservation) {
 
-        if (ReservationValidator.validateReservationParameters(reservation)) {
-            Set<Employee> employees = employeeDatabase.getAllItemsFromDatabase();
-
+        if (!ReservationValidator.validateReservationParameters(reservation)) {
+//throw exception - reservation fields are incorrect
         }
+
+        if (!EmployeeValidator.validateIfCurrentEmployee(reservation)) {
+//throw exception - employee is not working anymore
+        }
+
+        if (ReservationValidator.validateReservationTimeAvailable(reservation)) {
+//throw exception - this time is booked already
+        }
+
+        if (!ClientValidator.validateClientParameters(reservation)) {
+//throw exception - client fields are incorrect
+        }
+
+
+        if (!ClientValidator.validateIfCurrentClient(reservation)) {
+
+            clientDatabase.addToDatabase(reservation.getClient());
+
+        } else {
+
+            if (ClientValidator.validateClientHasReservationAtTheSameTime(reservation)) {
+                //throw exception - client has a reservation at the suggested time
+            }
+        }
+
         return reservation;
     }
 
