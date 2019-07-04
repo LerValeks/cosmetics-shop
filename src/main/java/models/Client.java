3daf8992f6e7112dff2209f88e40482c56d9f9ef
Danylo Tkachenko <d.y.tkachenko@gmail.com>
2019-07-04 17:05:46 +0200

    Employee and client classes updated

--- src/main/java/models/Client.java
+++ src/main/java/models/Client.java
@@ -1,7 +1,10 @@
 package models;
 
+import lombok.EqualsAndHashCode;
+
 import static models.UserCategory.CLIENT;
 
+@EqualsAndHashCode
 public class Client extends User {
 
     public Client(String name, String surname, String phoneNumber) {
--- src/main/java/models/Employee.java
+++ src/main/java/models/Employee.java
@@ -1,19 +1,28 @@
 package models;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import static models.EmploymentStatus.EMPLOYED;
 import static models.UserCategory.EMPLOYEE;
 
+@Setter
+@Getter
+@EqualsAndHashCode
 public class Employee extends User {
 
-    List<Reservation> reservations;
-
+    private List<Reservation> reservations;
+    private EmploymentStatus employmentStatus;
     private ServiceCategory serviceCategory;
 
     public Employee(String name, String surname, String phoneNumber, ServiceCategory serviceCategory) {
         super(name, surname, phoneNumber, EMPLOYEE);
         this.serviceCategory = serviceCategory;
         this.reservations = new ArrayList<>();
+        this.employmentStatus = EMPLOYED;
     }
 }
