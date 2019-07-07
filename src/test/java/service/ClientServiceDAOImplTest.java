5c7b5ff6b5130ade68474afd16a7e8a353df6311
Aleks <aleksvaluyskov@gmail.com>
2019-07-07 15:04:23 +0300

    Added Junit Employee and Client tests

---
+++ src/test/java/service/ClientServiceDAOImplTest.java
@@ -0,0 +1,28 @@
+package service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClientServiceDAOImplTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void reservationByClient() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void reservationByClientInSpecificPeriod() {
+    }
+}
---
+++ src/test/java/service/EmployeeServiceDAOImplTest.java
@@ -0,0 +1,36 @@
+package service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EmployeeServiceDAOImplTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void serviceCategoryEmployees() {
+    }
+
+    @Test
+    public void freeEmployees() {
+    }
+
+    @Test
+    public void reservationByEmployeeInSpecificPeriod() {
+    }
+
+    @Test
+    public void reservationByEmployeeByServiceCategoryInSpecificPeriod() {
+    }
+}
