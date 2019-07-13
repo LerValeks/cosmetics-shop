84b560e56d14a0c8c6e221062a8a8675bfd37fa8
Aleks <aleksvaluyskov@gmail.com>
2019-07-13 14:56:17 +0300

    employee validator modification

--- src/main/java/service/validation/EmployeeValidator.java
+++ src/main/java/service/validation/EmployeeValidator.java
@@ -13,8 +13,7 @@ public class EmployeeValidator {
     //TODO: Robert to advise how making employeedatabase object "Static" will affect the code? In reservation service this isn't static, shall be? Why in general it's prohibited calling statics from non-static context?
 
     public static boolean validateClientParameters(Employee employee) throws EmployeeException {
-        validateClientIsNull(employee);
-
+        validateEmployeeIsNull(employee);
                if ( validateEmployeeName(employee)
                 || validateEmployeeSurname(employee)
                 || validateEmployeePhone(employee)
@@ -24,7 +23,7 @@ public class EmployeeValidator {
         return true;
     }
 
-    public static boolean validateClientIsNull(Employee employee) throws EmployeeException  {
+    public static boolean validateEmployeeIsNull(Employee employee) throws EmployeeException  {
         if (employee==null) {
             throw new EmployeeException("Employee is null");
         }
