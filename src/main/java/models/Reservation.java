68a602cf22e8953adf23052e6af06b38b5ecf033
Danylo Tkachenko <d.y.tkachenko@gmail.com>
2019-07-14 03:16:01 +0200

    Reservation class ->> clean code

--- src/main/java/models/Reservation.java
+++ src/main/java/models/Reservation.java
@@ -6,10 +6,10 @@ import lombok.Setter;
 
 import java.time.LocalDate;
 
-//TODO: add reservation ID for easier search.
 @AllArgsConstructor
 @Getter
 @Setter
+
 public class Reservation {
 
     ServiceCategory serviceCategory;
