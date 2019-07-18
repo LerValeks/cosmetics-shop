024cc9c1f328029d85ec4facb334ff0faa0aa4d2
Danylo Tkachenko <d.y.tkachenko@gmail.com>
2019-07-18 22:00:07 +0200

    Reservation Service test class added

---
+++ src/test/java/service/ReservationServiceImplTest.java
@@ -0,0 +1,22 @@
+package service;
+
+import org.junit.Test;
+
+public class ReservationServiceImplTest {
+
+    @Test
+    public void makeReservation() {
+    }
+
+    @Test
+    public void showActiveReservations() {
+    }
+
+    @Test
+    public void showReservationsForSpecicTimePeriod() {
+    }
+
+    @Test
+    public void cancelClientReservation() {
+    }
+}
