1608435e99247ae17cc9812f4888a8dff42ca3d4
Danylo Tkachenko <d.y.tkachenko@gmail.com>
2019-07-18 21:14:36 +0200

    Client Validator finalized

--- src/test/java/service/validation/ClientValidatorTest.java
+++ src/test/java/service/validation/ClientValidatorTest.java
@@ -1,21 +1,101 @@
 package service.validation;
 
 import models.Client;
+import models.Employee;
+import models.Reservation;
+import models.ServiceCategory;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import repository.ClientDAO;
+import repository.EmployeeDAO;
 import service.exceptions.ClientException;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
 
 public class ClientValidatorTest {
 
+    @Mock
+    private ClientDAO clientDAO;
+
+    @Mock
+    private EmployeeDAO employeeDAO;
+
+    @InjectMocks
+    private ClientValidator clientValidator;
+
     private static Client createClient() {
-        return new Client("John", "Snow", "9379992");
+
+        return new Client("Alfa", "Client", "777 77 77");
+    }
+
+    private static Set<Client> createClients() {
+
+        Set<Client> clients = new HashSet<>();
+
+        for (int i = 0; i < 5; i++) {
+
+            Client client = createClient();
+            client.setPhoneNumber(Integer.toString(i));
+            clients.add(client);
+        }
+        return clients;
+    }
+
+    private static Employee createEmployee() {
+
+        return new Employee("Alfa", "Employee", "937 99 92", ServiceCategory.HAIRCUT);
+    }
+
+    private static Set<Employee> createEmployees() {
+
+        Set<Employee> employees = new HashSet<>();
+
+        for (int i = 0; i < 5; i++) {
+
+            Employee employee = createEmployee();
+            employee.setPhoneNumber(i + "e");
+            Set<Reservation> reservations = createReservations();
+            employee.getReservations().addAll(reservations);
+
+            employees.add(employee);
+        }
+        return employees;
+    }
+
+    private static Reservation createReservation(Employee employee, Client client) {
+
+        return new Reservation(ServiceCategory.HAIRCUT, employee, client, LocalDateTime.now());
+    }
+
+    private static Set<Reservation> createReservations() {
+
+        Set<Reservation> reservations = new HashSet<>();
+
+        for (int i = 0; i < 5; i++) {
+
+            Reservation reservation = createReservation(createEmployee(), createClient());
+            reservation.getEmployee().setPhoneNumber(Integer.toString(i));
+            reservation.getClient().setPhoneNumber(Integer.toString(i));
+            reservations.add(reservation);
+        }
+        return reservations;
     }
 
     @Test
-    public void validateClientParameters_shouldReturnTrueIfClientIsNotNullAndAllParametersAreNotNull() throws ClientException {
+    public void validateClientParameters_shouldReturnTrue_IfClientIsNotNullAndAllParametersAreNotNull() throws ClientException {
 
         //given
         Client client = createClient();
@@ -29,7 +109,7 @@ public class ClientValidatorTest {
     }
 
     @Test
-    public void validateClientParameters_shouldReturnFalseIfClientIsNull() throws ClientException {
+    public void validateClientParameters_shouldReturnFalse_IfClientIsNull() throws ClientException {
 
         //given
         Client client = null;
@@ -42,7 +122,7 @@ public class ClientValidatorTest {
     }
 
     @Test
-    public void validateClientParameters_shouldReturnFalseIfClientHasOneNullParameter() throws ClientException {
+    public void validateClientParameters_shouldReturnFalse_IfClientHasOneNullParameter() throws ClientException {
 
         //given
         Client client1 = createClient();
@@ -70,13 +150,100 @@ public class ClientValidatorTest {
         assertFalse(clientValidation3);
     }
 
-    //TODO: Require mock static object DAO - mocking not allowed with statics. Robert to advise
     @Test
-    public void validateIfCurrentClient() {
+    public void validateIfExistingClient_ShouldReturnTrue_IfClientExistsInClientDAO() throws ClientException {
+
+        //given
+        Set<Client> clients = createClients();
+        Client client = createClient();
+        clients.add(client);
+
+        //when
+        Mockito.when(clientDAO.getAllItems()).thenReturn(clients);
+        boolean clientAvailableInDAO = clientValidator.validateIfExistingClient(client);
+
+        //then
+        Assert.assertTrue(clientAvailableInDAO);
+        assertEquals(6, clients.size());
     }
 
-    //TODO: Require mock static object DAO - mocking not allowed with statics. Robert to advise
     @Test
-    public void validateClientHasReservationAtTheSameTime() {
+    public void validateIfExistingClientHasReservationAtTheSameTime_shouldReturnTrue_IfSameTimeExists() throws ClientException {
+
+        //given
+        Set<Employee> allEmployees = createEmployees();
+
+        Client client = createClient();
+        client.setName("Checked");
+        client.setSurname("Client");
+        client.setPhoneNumber("3658990");
+
+        Employee employeeOne = createEmployee();
+        employeeOne.setName("First");
+        employeeOne.setSurname("Employee");
+
+        Employee employeeTwo = createEmployee();
+        employeeTwo.setName("Second");
+        employeeTwo.setSurname("Employee");
+
+
+        Reservation existingReservation = createReservation(employeeOne, client);
+        existingReservation.setReservationTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(15, 30)));
+
+        Reservation newReservation = createReservation(employeeTwo, client);
+        newReservation.setReservationTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(15, 30)));
+
+        employeeOne.getReservations().add(existingReservation);
+
+        allEmployees.add(employeeOne);
+
+
+        //when
+        Mockito.when(employeeDAO.getAllItems()).thenReturn(allEmployees);
+        boolean clientTimeCheck = clientValidator.validateIfExistingClientHasReservationAtTheSameTime(newReservation);
+
+        //then
+        Assert.assertNotNull(clientTimeCheck);
+        Assert.assertTrue(clientTimeCheck);
+    }
+
+    @Test
+    public void validateIfExistingClientHasReservationAtTheSameTime_shouldReturnFalse_IfSameTimeDoesNotExist() throws ClientException {
+
+        //given
+        Set<Employee> allEmployees = createEmployees();
+
+        Client client = createClient();
+        client.setName("Checked");
+        client.setSurname("Client");
+        client.setPhoneNumber("3658990");
+
+        Employee employeeOne = createEmployee();
+        employeeOne.setName("First");
+        employeeOne.setSurname("Employee");
+
+        Employee employeeTwo = createEmployee();
+        employeeTwo.setName("Second");
+        employeeTwo.setSurname("Employee");
+
+
+        Reservation existingReservation = createReservation(employeeOne, client);
+        existingReservation.setReservationTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(15, 30)));
+
+        Reservation newReservation = createReservation(employeeTwo, client);
+        newReservation.setReservationTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(15, 31)));
+
+        employeeOne.getReservations().add(existingReservation);
+
+        allEmployees.add(employeeOne);
+
+
+        //when
+        Mockito.when(employeeDAO.getAllItems()).thenReturn(allEmployees);
+        boolean clientTimeCheck = clientValidator.validateIfExistingClientHasReservationAtTheSameTime(newReservation);
+
+        //then
+        Assert.assertNotNull(clientTimeCheck);
+        Assert.assertFalse(clientTimeCheck);
     }
 }
